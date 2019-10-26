05e6689a9024d42341bd34de5c535c27290e682e
Nathaniel Gordon <gordonng123@gmail.com>
2019-10-26 15:21:19 -0400

    new database-ready format

--- data/to_database.py
+++ data/to_database.py
@@ -1,7 +1,7 @@
 import json
 with open("sent_python/newout.json", "r") as in_file:
     data = json.load(in_file)
-result = [json.dumps(record) for record in data]
+result = [json.dumps(record) for record in data['items']]
 with open("data/database_ready.txt", 'w') as out:
     for i in result:
         out.write(i+'\n')
