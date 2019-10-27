75d737d08f94a0c9977b7131624d05cb67605558
YaleDuffy <yaleduffy@hotmail.com>
2019-10-27 00:53:27 -0400

    final southwest + delta database

--- data/to_database.py
+++ data/to_database.py
@@ -1,7 +1,7 @@
 import json
-with open("sent_python/newout.json", "r", encoding='utf-8') as in_file:
+with open("southwest_output_data.json", "r") as in_file:
     data = json.load(in_file)
 result = [json.dumps(record) for record in data['items']]
-with open("data/database_ready.txt", 'w', encoding="utf-8") as out:
+with open("southwest_database_ready.txt", 'w') as out:
     for i in result:
         out.write(i+'\n')
